c7fc3385ff38f4e73d97a842951b562c0f6f04ba
belajar-vue-2 <aryaadi229@gmail.com>
2025-12-07 13:18:09 +0700

    feat: Add tests for NoteList component with mount and render success cases

--- tests/Feature/Livewire/Note/NoteListTest.php
+++ tests/Feature/Livewire/Note/NoteListTest.php
@@ -1,7 +1,39 @@
 <?php
 
-test('example', function () {
-    $response = $this->get('/');
+use App\Livewire\NoteList;
+use App\Models\MetaData;
+use App\Models\Note;
+use Database\Seeders\DatabaseSeeder;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Facades\Storage;
+use Livewire\Livewire;
 
-    $response->assertStatus(200);
+uses(RefreshDatabase::class);
+
+test('mount success', function () {
+    Storage::fake('public');
+
+    $this->seed(DatabaseSeeder::class);
+
+    $meta_data = MetaData::first();
+
+    Livewire::test(
+        NoteList::class,
+        ['meta_data_id' => $meta_data->id]
+    )
+        ->assertSet('meta_data_id', $meta_data->id);
+});
+
+test('render success', function () {
+    Storage::fake('public');
+
+    $this->seed(DatabaseSeeder::class);
+
+    $meta_data = MetaData::first();
+    $note = Note::first();
+
+    $component = Livewire::test(
+        NoteList::class,
+        ['meta_data_id' => $meta_data->id]
+    )->assertSeeText($note->message);
 });
