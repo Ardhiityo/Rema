07590a993ba6ea90f667703c4961ae97fdc8e44e
Ardhityo <aryaadi229@gmail.com>
2026-06-13 16:52:34 +0700

    refactor: remove unused MetadataPolicy class

---
+++ app/Policies/MetadataPolicy.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\Metadata;
+use App\Models\User;
+
+class MetadataPolicy
+{
+    /**
+     * Determine whether the user can view any models.
+     */
+    public function viewAny(User $user): bool
+    {
+        return false;
+    }
+
+    /**
+     * Determine whether the user can view the model.
+     */
+    public function view(User $user, Metadata $metadata): bool
+    {
+        if ($user->hasRole('author')) {
+            return $user->author->id == $metadata->author_id;
+        }
+
+        return true;
+    }
+
+    /**
+     * Determine whether the user can create models.
+     */
+    public function create(User $user): bool
+    {
+        return false;
+    }
+
+    /**
+     * Determine whether the user can update the model.
+     */
+    public function update(User $user, Metadata $metadata): bool
+    {
+        if ($user->hasRole('author')) {
+            return $user->author->id == $metadata->author_id && $metadata->status != 'approve';
+        } elseif ($user->hasRole('leader')) {
+            return false;
+        }
+
+        return true;
+    }
+
+    /**
+     * Determine whether the user can delete the model.
+     */
+    public function delete(User $user, Metadata $metadata): bool
+    {
+        if ($user->hasRole('author')) {
+            return $user->author->id == $metadata->author_id && $metadata->status != 'approve';
+        } elseif ($user->hasRole('leader')) {
+            return false;
+        }
+
+        return true;
+    }
+
+    /**
+     * Determine whether the user can restore the model.
+     */
+    public function restore(User $user, Metadata $metadata): bool
+    {
+        return false;
+    }
+
+    /**
+     * Determine whether the user can permanently delete the model.
+     */
+    public function forceDelete(User $user, Metadata $metadata): bool
+    {
+        return false;
+    }
+}
