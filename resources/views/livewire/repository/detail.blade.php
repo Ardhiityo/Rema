05d5657aa95c39f897bcea0bd59b14db4108fbe4
Arya Adhi Prasetyo <ardhityo229@gmail.com>
2026-07-20 17:35:21 +0700

    style: enhance avatar display and improve category link formatting in detail view

--- resources/views/livewire/repository/detail.blade.php
+++ resources/views/livewire/repository/detail.blade.php
@@ -8,8 +8,9 @@
             <div class="card-body">
                 <div class="flex-column d-flex">
                     <h4 class="gap-2 card-title d-flex align-items-center">
-                        <img src="{{ $avatar }}" alt="{{ $meta_data->author_name }}" class="rounded-pill"
-                            style="max-width: 38px">
+                        <div class="avatar avatar-lg">
+                            <img src="{{ $avatar }}" alt="avatar">
+                        </div>
                         <span>{{ $meta_data->author_name }}</span>
                     </h4>
                     <p class="mb-0">
@@ -39,16 +40,15 @@
                         @endforeach
                     </small>
                 </p>
-                <div class="gap-3 d-flex mt-4">
+                <div class="gap-3 mt-4 d-flex">
                     @foreach ($meta_data->categories as $category)
-                        <a href="{{ route('repository.read', [
+                                        <a href="{{ route('repository.read', [
                             'category_slug' => $category->slug,
                             'meta_data_slug' => $meta_data->slug,
-                        ]) }}"
-                            target="_blank" class="btn btn-info btn-sm">
-                            <span>{{ $category->name }}</span>
-                            <i class="bi bi-eye-fill"></i>
-                        </a>
+                        ]) }}" target="_blank" class="btn btn-info btn-sm">
+                                            <span>{{ $category->name }}</span>
+                                            <i class="bi bi-eye-fill"></i>
+                                        </a>
                     @endforeach
                 </div>
             </div>
@@ -56,7 +56,7 @@
     </div>
 
     @hasrole('admin')
-        <livewire:note.note-form :meta_data_id="$meta_data->id" />
+    <livewire:note.note-form :meta_data_id="$meta_data->id" />
     @endhasrole
     @can('view', $meta_data)
         <livewire:note.note-list :meta_data_id="$meta_data->id" />
